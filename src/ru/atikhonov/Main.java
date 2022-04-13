dee9057365cf4a2e0e51a7391daa82a574fa207f
a2tikhonov <git!St@d1um@rc@d1um>
2022-04-13 20:01:14 +0300

    Сделал домашку

--- src/ru/atikhonov/Main.java
+++ src/ru/atikhonov/Main.java
@@ -5,6 +5,19 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
-
+        //Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        //Задание 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase(Locale.ROOT));
+        //Задание 3
+        fullName = "Иванов Семён Семёныч";
+        if (fullName.contains("ё")) {
+            fullName = fullName.replace("ё", "е");
+        }
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
